c0880cc15a1c7969fd4a7c6df793e687d5ab9465
Daniel Acosta <a01736279@tec.mx>
2023-10-31 17:37:53 -0600

    Primer commit (fue un desmadre)

---
+++ PueblaReecicla/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  PueblaReecicla
+//
+//  Created by Administrador on 31/10/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
