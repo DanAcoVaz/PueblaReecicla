1ac42745d3938ac1a9aebade7a28186cc4d1d318
Daniel Acosta <a01736279@tec.mx>
2023-11-03 15:25:54 -0600

    Added Alerts confirmation alerts in sign up (YA SE QUE ES SPANGLISH)

--- PueblaReecicla/SignUpVC.swift
+++ PueblaReecicla/SignUpVC.swift
@@ -22,7 +22,6 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var lastnameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var phoneTxt: UITextField!
-    @IBOutlet weak var bdayTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var confirmpwdTxt: UITextField!
     
@@ -45,5 +44,36 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    @IBAction func signUpTap(_ sender: Any) {
+        let alert = UIAlertController(title: "¿Confirmar registro?", message: "¿Desea confirmar el registro de su cuenta?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancelar", comment: "Default action"), style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Confirmar", style: .default, handler: { action in
+            
+            self.confirmSignUp()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func confirmSignUp() {
+        let confirmAlert = UIAlertController(title: "Activa tu cuenta", message: "Te hemos enviado un correo de confirmación con un código al correo que proporcionaste. Introduce el código a continuación para activar tu cuenta:" , preferredStyle: .alert)
+        confirmAlert.addTextField { (textField) in
+            textField.text = ""
+        }
+        confirmAlert.addAction(UIAlertAction(title: "Activar", style: .default, handler: { [weak confirmAlert] (_) in
+            let textField = confirmAlert?.textFields![0]
+            //Acciones a realizar al pulsar el boton activar en la alerta:
+            
+            //print("Text field: \(textField?.text)")
+            
+            //Mostrar que se ha activado la cuenta
+            
+            let activeAlert = UIAlertController(title: "Cuenta Activada", message: "Listo! Has concluido el proceso de registro. Puedes comenzar a usar la aplicación", preferredStyle: .alert)
+            activeAlert.addAction(UIAlertAction(title: "Continuar", style: .default, handler: nil))
+            self.present(activeAlert, animated: true)
+        }))
+        self.present(confirmAlert, animated: true, completion: nil)
+    }
+    
 
 }
