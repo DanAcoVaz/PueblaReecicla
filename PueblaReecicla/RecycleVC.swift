445c6ebc6a923dc05dfbc29425cef6c579e8fdb9
Daniel Acosta <a01736279@tec.mx>
2023-11-08 14:02:35 -0600

    Finished Tab bar functionality and separation of main screens

---
+++ PueblaReecicla/RecycleVC.swift
@@ -0,0 +1,18 @@
+//
+//  RecycleVC.swift
+//  PueblaReecicla
+//
+//  Created by David Bo on 08/11/23.
+//
+
+import UIKit
+
+class RecycleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ PueblaReecicla/SettingsVC.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsVC.swift
+//  PueblaReecicla
+//
+//  Created by David Bo on 08/11/23.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
