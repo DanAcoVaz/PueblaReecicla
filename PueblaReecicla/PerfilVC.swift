d06120f82eb4328db26851b4aa937511707e4020
Daniel Acosta <a01736279@tec.mx>
2023-11-23 12:54:02 -0600

    Avances profile (actualizacion de datos y logout)

--- PueblaReecicla/PerfilVC.swift
+++ PueblaReecicla/PerfilVC.swift
@@ -31,7 +31,28 @@ class PerfilViewController: UIViewController, UITextFieldDelegate {
         
         if let currentUser = Auth.auth().currentUser{
             let uid = currentUser.uid
-            let userRef = Firestore.firestore().collection("usuarios").document(uid)
+            let docRef = Firestore.firestore().collection("usuarios").document(uid)
+            docRef.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    if let nombre = document.data()?["nombre"] as? String {
+                        self.nameTxt.text = nombre
+                    }
+                    if let apellido = document.data()?["apellidos"] as? String {
+                        self.lastnameTxt.text = apellido
+                    }
+                    if let correo = document.data()?["correo"] as? String {
+                        self.emailTxt.text = correo
+                    }
+                    if let telefono = document.data()?["telefono"] as? String {
+                        self.phoneTxt.text = telefono
+                    }
+                    if let bday = document.data()?["fechaNacimiento"] as? Timestamp {
+                        self.bdayPicker.date = bday.dateValue()
+                    }
+                } else {
+                    print("Error with retrieving user info. Perhaps user (document) doesn't exist?")
+                }
+            }
         } else {
             print("Error al recuperar informacion")
         }
@@ -45,5 +66,71 @@ class PerfilViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    func setDestructiveAlert(alertTitle: String, alertMessage: String){
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .destructive))
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func updateDataTap(_ sender: Any) {
+        if (nameTxt.text!.isEmpty || lastnameTxt.text!.isEmpty || emailTxt.text!.isEmpty || phoneTxt.text!.isEmpty) {
+            setDestructiveAlert(alertTitle: "Error", alertMessage: "Hay campos vacios")
+        } else if !emailTxt.isValidEmail() {
+            setDestructiveAlert(alertTitle: "Error", alertMessage: "Correo electrónico no válido")
+        } else if !phoneTxt.isValidPhoneNumber() {
+            setDestructiveAlert(alertTitle: "Error", alertMessage: "Número de teléfono inválido. Debe tener 10 dígitos.")
+        } else {
+            let alert = UIAlertController(title: "¿Confirmar cambios?", message: "¿Deseas realizar estos cambios a tu cuenta?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("Cancelar", comment: "Default action"), style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Confirmar", style: .default, handler: { action in
+                self.updateData()
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func updateData(){
+        let data: [String: Any] = [
+            "nombre": nameTxt.text!,
+            "apellidos": lastnameTxt.text!,
+            "correo": emailTxt.text!,
+            "telefono": phoneTxt.text!,
+            "fechaNacimiento": bdayPicker.date
+        ]
+        if let currentUser = Auth.auth().currentUser {
+            let uid = currentUser.uid
+            Firestore.firestore().collection("usuarios").document(uid).updateData(data) { err in
+                if let err = err {
+                    print("Error updating document: \(err) ")
+                }
+                else {
+                    print("Document successfully updated")
+                }
+                currentUser.updateEmail(to: self.emailTxt.text!) { error in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        print("CHANGED")
+                    }
+                }
+                
+            }
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func closeSessionTap(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "ViewController")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        } catch let signOutError as NSError {
+            print("Error al cerrar sesión en firebase: \(signOutError)")
+        }
+    }
+    
 
 }
